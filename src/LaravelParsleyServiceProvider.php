e4cd8051e25357fa19123ab65c5d4bba936eede3
MaDnh <dodanhmanh@gmail.com>
2017-05-03 16:29:24 +0700

    Fix bug method not found in Laravel 5.4
    Method Session::getToken removed, use Session::token instead

--- src/LaravelParsleyServiceProvider.php
+++ src/LaravelParsleyServiceProvider.php
@@ -20,7 +20,7 @@ class LaravelParsleyServiceProvider extends HtmlServiceProvider
     protected function registerFormBuilder()
     {
         $this->app->singleton('form', function ($app) {
-            $form = new FormBuilder($app['html'], $app['url'], $app['view'], $app['session.store']->getToken());
+            $form = new FormBuilder($app['html'], $app['url'], $app['view'], $app['session.store']->token());
 
             return $form->setSessionStore($app['session.store']);
         });
